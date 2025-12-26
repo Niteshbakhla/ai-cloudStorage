5f4d31b23be6512f24f5d53d11e59197401fff7c
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-26 23:06:21 +0530

    config axios

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-            baseURL: `https://ai-cloudstorage.onrender.com/api/v1`,
+            baseURL: `${import.meta.env.VITE_API_URL}api/v1`,
             withCredentials: true
 });
-
 export default axiosInstance;
