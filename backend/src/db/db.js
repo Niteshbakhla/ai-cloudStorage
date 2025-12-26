ea0dc93156e6605bee76dc228a857504e9b16213
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-26 21:40:02 +0530

    updated a db configuration

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,19 +1,21 @@
 import mongoose from "mongoose";
 import config from "../config/config.js";
 
+const URL = config.NODE_ENV === "production"
+            ? config.MONGO_CLOUD_URL
+            : config.MONGO_LOCAL_URL;
 
-const URL = config.NODE_ENV === "production" ? config.MONGO_CLOUD_URL : config.MONGO_LOCAL_URL;
 const connectDB = async () => {
             try {
                         const connect = await mongoose.connect(URL);
                         if (connect.connection.readyState === 1) {
-                                    console.log("Database is connected!")
+                                    console.log("✅ Database is connected!");
                         } else {
-                                    console.log("Database is not connected!")
+                                    console.log("❌ Database is not connected!");
                         }
             } catch (error) {
-                        console.log("DB error:-", error);
+                        console.log("DB error:", error);
             }
-}
+};
 
 export default connectDB;
